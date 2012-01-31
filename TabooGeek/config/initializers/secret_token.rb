be8a95924de19eb1b78385345f6dbe98cbf3e1c1
descl <descl@zouig.org>
2012-01-31 09:05:28 +0100

    add empty project

---
+++ TabooGeek/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabooGeek::Application.config.secret_token = '642fbd7de39dfc143fcbd8c5dcad7604b71515b0a1d624b322aa01eaa47bb9ea9a290028ca299f887791f5ad55b5d53af63c8489e40ed813f0d64a89ac441fd7'
