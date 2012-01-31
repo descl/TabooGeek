50be76033f61e6cc214f7844a64ae8cb5affc6a0
descl <descl@zouig.org>
2012-01-31 10:01:26 +0100

    add pages for iteration1

--- TabooGeek/config/routes.rb
+++ TabooGeek/config/routes.rb
@@ -1,4 +1,8 @@
 TabooGeek::Application.routes.draw do
+  get "game/game"
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,8 @@ TabooGeek::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "home#index"
+
 
   # See how all your routes lay out with "rake routes"
 
