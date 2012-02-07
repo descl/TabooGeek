10ad5568c526463486d8dbc4eabc062901ee1bc9
Rubix <fabien.brossier@gmail.com>
2012-02-07 17:27:21 +0100

    Modified SQPARL query.

--- TabooGeek/app/controllers/game_controller.rb
+++ TabooGeek/app/controllers/game_controller.rb
@@ -9,8 +9,15 @@ class GameController < ApplicationController
     
     
     
-    query = 'SELECT  ?thing ?a ?type WHERE { ?thing ?a ?type . } ORDER BY ?type'
-    endpoint = 'http://localhost:8080/sparql/'
+    #query = 'SELECT ?thing ?a ?type WHERE { ?thing ?a ?type }'
+    
+    query =  'PREFIX skos: <http://www.w3.org/2004/02/skos/core#>'
+    query += 'SELECT ?altLabel WHERE {'
+    query += '?concept skos:prefLabel ?prefLabel' 
+    query += '?concept skos:altLabel ?altLabel'
+    query += 'FILTER regex(?prefLabel, "' + @game.capitalize + '")}'
+    
+    endpoint = 'http://157.169.101.31:8080/sparql/'
     response = RestClient.post endpoint, :query => query
     @xml = Nokogiri::XML(response.to_str)
     
