9a37326c81baa9738e079ba15227d768ca00662f
Rubix <fabien.brossier@gmail.com>
2012-02-07 18:13:38 +0100

    First version of the words list.

--- TabooGeek/app/controllers/home_controller.rb
+++ TabooGeek/app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    
+    query =  "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n"
+    query += "SELECT ?prefLabel WHERE {\n"
+    query += "?concept skos:prefLabel ?prefLabel }"
+    
+    endpoint = 'http://157.169.101.31:8080/sparql/'
+    response = RestClient.post endpoint, :query => query
+    xml = Nokogiri::XML(response.to_str)
+    @words = xml.xpath('//sparql:binding[@name = "prefLabel"]/sparql:literal', 'sparql' => 'http://www.w3.org/2005/sparql-results#')
   end
 end
