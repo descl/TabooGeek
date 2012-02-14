3700de1fbc7eb2a49d3fced5a3acfd7b7207d351
descl <descl@zouig.org>
2012-02-14 14:44:24 +0100

    changement lib de RDF

--- TabooGeek/app/controllers/game_controller.rb
+++ TabooGeek/app/controllers/game_controller.rb
@@ -12,18 +12,17 @@ class GameController < ApplicationController
     
     query =  "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n"
     query += "SELECT ?altLabel WHERE {\n"
-    query += "?concept skos:prefLabel ?prefLabel ."
-    query += "?concept skos:altLabel ?altLabel ."
+    query += "?concept <http://www.w3.org/2004/02/skos/core#prefLabel> ?prefLabel ."
+    query += "?concept <http://www.w3.org/2004/02/skos/core#altLabel> ?altLabel ."
     query += "FILTER regex(?prefLabel, '" + @game.capitalize + "')}"
     
     @tQuery = query
     
     endpoint = 'http://157.169.101.31:8080/sparql/'
-    response = RestClient.post endpoint, :query => query
-    @xml = Nokogiri::XML(response.to_str)
-    @words = @xml.xpath('//sparql:binding[@name = "altLabel"]/sparql:literal', 'sparql' => 'http://www.w3.org/2005/sparql-results#')
-    
-    
+ 
+    store = FourStore::Store.new endpoint
+    @words = store.select(query)
+    print @words
   end
   
 end
